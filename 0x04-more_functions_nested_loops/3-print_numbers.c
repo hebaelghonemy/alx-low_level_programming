6f3857af926d548eb3bdf647ddbfd35fe4a1ea6e
hebaelghonemy <hebaelghonemy8@gmail.com>
2023-04-27 10:05:30 -0700

    3. The numbers speak for themselves

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* print_numbers - function that prints the numbers, from 0 to 9
+*@i:entered numder
+*/
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i > 10; i++)
+	{
+		_putchar ('i');
+
+		_putchar ('\n');
+	}
+}
