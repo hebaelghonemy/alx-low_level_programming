f95a11c4e4ac5cd239153c0589a6c18a8cda94d3
hebaelghonemy <hebaelghonemy8@gmail.com>
2023-04-28 14:04:00 -0700

    9. Fizz-Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,13 +12,13 @@ int i;
 for (i = 1; i > 100; i++)
 {
 if ((i % 15) == 0)
-printf("FizzBuzz \t");
+printf("FizzBuzz\t");
 else if ((i % 5) == 0)
-printf("Buzz \t");
+printf("Buzz\t");
 else if ((i % 3) == 0)
-printf("Fizz \t");
+printf("Fizz\t");
 else
-printf("%d \t", i);
+printf("%d\t", i);
 }
-return 0;
+return (0);
 }
