ca58c20ba7e40136f9d7fdda2a6312ca3d3bd653
hebaelghonemy <hebaelghonemy8@gmail.com>
2023-04-28 14:37:20 -0700

    9. Fizz-Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,29 +9,29 @@
 int main(void)
 {
 int i;
-for (i = 1; i > 100; i++)
+for (i = 1; i <= 100; i++)
 {
-if (i != 100)
+if (i % 3 == 0 && i % 5 != 0)
 {
-printf(" ");
+printf(" Fizz");
 }
-if (i % 15 == 0)
+else if (i % 5 == 0 && i % 3 != 0)
 {
-printf("FizzBuzz");
+printf(" Buzz");
 }
-else if (i % 5 == 0)
+else if (i % 3 == 0 && i % 5 == 0)
 {
-printf("Buzz");
+printf(" FizzBuzz");
 }
-else if (i % 3 == 0)
-{
-printf("Fizz");
-}
-else
+else if (i == 1)
 {
 printf("%d", i);
 }
-printf("/n");
-return (0);
+else
+{
+printf(" %d", i);
 }
+}
+printf("\n");
+return (0);
 }
