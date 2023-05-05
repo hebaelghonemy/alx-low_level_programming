18878f741dff4fd4c61ceede5391b8690898a898
hebaelghonemy <hebaelghonemy8@gmail.com>
2023-05-05 07:30:07 -0700

    104-print_buffer

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,13 +1,14 @@
 
 #include <stdio.h>
+
 int main(void)
 
 {
 int n;
 int a[5];
 int *p;
-a[2] = 1024;
 
+a[2] = 1024;
 p = &n;
 
   /*
@@ -17,11 +18,15 @@ p = &n;
    * - you are not allowed to modify p
    * - only one statement
    * - you are not allowed to code anything else than this line of code
-   */
+
+code
+
+*/
+*(p + 5) = 98;
+/* ...so that this prints 98\n */
 
 p[5] = 98;
-
-/* ...so that this prints 98\n */
 printf("a[2] = %d\n", a[2]);
 return (0);
 }
+
