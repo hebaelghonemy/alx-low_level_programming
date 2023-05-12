ff3739adf962ee24e1e1181eb092803fe39c3725
hebaelghonemy <hebaelghonemy8@gmail.com>
2023-05-12 13:02:55 -0700

    libmy.a

---
+++ 0x09-static_libraries/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* _isupper - function that check for  uppercase character
+* @c: the character in ASCII table
+* Return:1 for  uppercase. 0  for otherwise
+*/
+int _isupper(int c)
+{
+
+if (c >= 65 && c <= 90)
+
+	return (1);
+
+else
+
+	return (0);
+}
